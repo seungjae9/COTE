5e6fcc4e485bd496292a7be75a20878f6934fc62
ssafyseungjae <moseung18@gmail.com>
2020-04-08 12:36:46 +0900

    1592번

---
+++ SW대비/1592번 영식이와 친구들.py
@@ -0,0 +1,16 @@
+N, M, L = map(int, input().split())
+check = [1] + [0] * (N-1)
+ball = 0
+while M not in check:
+    if check[ball] % 2 != 0:
+        ball += L
+        if ball >= N:
+            ball -= N
+        check[ball] += 1
+    else:
+        ball -= L
+        if ball < -N:
+            ball += N
+        check[ball] += 1
+print(sum(check)-1)
+
