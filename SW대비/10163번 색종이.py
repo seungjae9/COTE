682088885a76bcbb5ad3ad73b1ae1041bf2679df
ssafyseungjae <moseung18@gmail.com>
2020-04-10 02:26:25 +0900

    색종이

---
+++ SW대비/10163번 색종이.py
@@ -0,0 +1,14 @@
+# 10163번 색종이
+
+N = int(input())
+paper = []
+for i in range(N):
+    paper.append(list(map(int, input().split())))
+
+board = [[0] * 101 for _ in range(101)]
+
+
+for i in range(N):
+    for j in range()
+    board[paper[i][0]][paper[i][1]] = 1
+
