1ea025e5b4995bba1d664609f52d89ae0882e9c9
ssafyseungjae <moseung18@gmail.com>
2020-04-20 22:17:27 +0900

    2422번 한윤정 아이스크림

--- SW대비/2422번 한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
+++ SW대비/2422번 한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
@@ -1,45 +1,21 @@
 # 2422번 한윤정이 이탈리아에 가서 아이스크림을 사먹는데
 
+N, M = map(int, input().split())
+x = [list(map(int, input().split())) for _ in range(M)]
 
+number = [[0] * N for _ in range(N)]
 
-# N, M = map(int, input().split())
-# x = [list(map(int, input().split())) for _ in range(M)]
-#
-# number = []
-# for i in range(N):
-#     number.append(i+1)
-#
-#
-# case = [[]]
-#
-# for i in range(N-2):
-#     for j in range(i+1, N-1):
-#         for x in range(j+1, N):
-#             # print(number[i], number[j], number[x])
-#
+# 방문 개념
+for i in range(M):
+    number[x[i][0] - 1][x[i][1] - 1], number[x[i][1] - 1][x[i][0] - 1] = "x", "x"
 
-
-n,m = map(int,input().split())
-a = [[False]*n for _ in range(n)]
-for _ in range(m):
-    u,v = map(int,input().split())
-    a[u-1][v-1] = a[v-1][u-1] = True
-ans = 0
-# print(a)
-for i in range(len(a)):
-    print(a[i])
-for i in range(0, n-2):
-    for j in range(i+1, n-1):
-        for k in range(j+1, n):
-            if a[i][j] or a[j][k] or a[k][i]:
-                # print(a[i][j])
-                # print(a[j][k])
-                # print(a[k][i])
-                # print()
+res = 0
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for x in range(j+1, N):
+            # 컨티뉴는 pass와 다르게 계속 해서 다음번 for문 loop를 호출
+            if number[i][j] or number[j][x] or number[x][i] == "x":
                 continue
-            print(a[i][j])
-            print(a[j][k])
-            print(a[k][i])
-            print()
-            ans += 1
-print(ans)
+            res += 1
+print(res)
+
