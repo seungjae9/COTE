ca4904da7e81c96337496ab0985d0e794fd38a14
ssafyseungjae <moseung18@gmail.com>
2020-04-30 21:03:58 +0900

    3805번 사탕게임

--- SW대비/3190번 뱀.py
+++ SW대비/3190번 뱀.py
@@ -1,2 +1,31 @@
 # 3190번 뱀
 
+N = int(input())
+K = int(input())
+board = [[0] * N for _ in range(N)]
+for i in range(K):
+    x, y = map(int, input().split())
+    board[x-1][y-1] = 1
+
+L = int(input())
+direction = [list(input().split()) for _ in range(L)]
+
+x = 0
+y = 0
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+for i in range(len(board)):
+    print(board[i])
+compass = 1   # 1은 동쪽, 2는 남쪽 3은 서쪽 4는 북쪽
+
+for case in range(L):
+    if direction[case][1] == "D":
+        compass = (compass + 1) % 4
+    elif direction[case][1] == "L":
+        compass = (compass + 3) % 4
+
+
+
+
--- SW대비/3805번 사탕 게임.py
+++ SW대비/3805번 사탕 게임.py
@@ -1,38 +1,48 @@
-# 3805번 사탕 게임
-
+# 3085번 사탕 게임
 N = int(input())
-origin = [list(input()) for _ in range(N)]
+board = [list(input()) for _ in range(N)]
 
-board = [[0] * N for _ in range(N)]
+def cnady():
+    global candymax
+    # 가로 바뀐거 하나씩 다 체크
+    for col in range(N):
+        tempmax = 1
+        for row in range(N - 1):
+            if board[col][row] == board[col][row + 1]:
+                tempmax += 1
+            else:
+                if candymax < tempmax:
+                    candymax = tempmax
+                tempmax = 1
+        if candymax < tempmax:
+            candymax = tempmax
 
-# 가로 바꾸기
+    # 세로 바뀐거 하나씩 다 체크
+    for row in range(N):
+        tempmax = 1
+        for col in range(N - 1):
+            if board[col][row] == board[col + 1][row]:
+                tempmax += 1
+            else:
+                if candymax < tempmax:
+                    candymax = tempmax
+                tempmax = 1
+
+        if candymax < tempmax:
+            candymax = tempmax
+
+candymax = 0
 for i in range(N):
     for j in range(N-1):
-
-        for col in range(N):
-            for row in range(N):
-                board[col][row] = origin[col][row]
-
+        #  바꾸기
         board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
-
-        compare1 = 0
-        compare2 = 0
-
-        initial = board[i][j]
-
-
-        check = 1
-        for x in range(i, N):
-            if x - check >= 0:
-                if board[x-check][j] == initial:
-                    compare1 += 1
-            if x + check <= N - 1:
-                if board[x+check][j] == initial:
-                    compare2 += 1
-            check += 1
-
-
-        print(compare1 + compare2)
-
-
-
+        # 바뀐거 체크
+        cnady()
+        # 원상복구
+        board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
+        # # 바꾸기
+        board[j][i], board[j+1][i] = board[j+1][i], board[j][i]
+        cnady()
+        # 원상복구
+        board[j][i], board[j+1][i] = board[j+1][i], board[j][i]
+print(candymax)
