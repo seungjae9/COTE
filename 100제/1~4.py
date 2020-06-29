3a9c8b5634158fb1e6511f32dd6a5f6e5e5d267e
seungjae9 <moseung18@gmail.com>
2020-06-29 18:00:15 +0900

    1~5번

---
+++ 100제/1~4.py
@@ -0,0 +1,14 @@
+nums = [100, 200, 300, 400, 500]
+# nums.remove(400)
+# nums.remove(500)
+# del nums[-1]
+# del nums[-1]
+nums.pop()
+nums.pop()
+print(nums)
+
+
+
+l = [200, 100, 300]
+l.insert(2, 1000)
+print(l)
---
+++ 100제/5.py
@@ -0,0 +1,15 @@
+for i in range(1, 11, 2):
+    print(i)
+# 1 부터 +2 커지면서 11미만 까지 출력
+# 1, 3, 5, 7, 9
+# 11(포함 x)
+
+for i in range(1, 5):
+    print(i)
+# 1부터 5미만 까지 출력
+
+for i in range(5):
+    print(i)
+# 0부터 5개 출력
+
+# range 범위는 0부터 시작하는데 x, y, z 이렇게 범위를 정할수있다.
